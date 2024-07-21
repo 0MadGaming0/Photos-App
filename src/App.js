37a676d6fd41d4dce873670fa7715165812ad66c
0MadGaming0 <madhavsp05@gmail.com>
2024-07-21 13:48:49 +0530

    Photos App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddPhotos from './components/AddPhotos';
 import SearchPhotos from './components/SearchPhotos';
+import Viewall from './components/Viewall';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<AddPhotos/>}/>
       <Route path="/search" element={<SearchPhotos/>}/>
+      <Route path="/view" element={<Viewall/>}/>
     </Routes>
     </BrowserRouter>
   );
