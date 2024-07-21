15b7a4dc5ff0b7baf7818fd51e46b8b964e6ba5a
0MadGaming0 <madhavsp05@gmail.com>
2024-07-21 13:18:59 +0530

    Photos App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddPhotos from './components/AddPhotos';
+import SearchPhotos from './components/SearchPhotos';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<AddPhotos/>}/>
+      <Route path="/search" element={<SearchPhotos/>}/>
     </Routes>
     </BrowserRouter>
   );
